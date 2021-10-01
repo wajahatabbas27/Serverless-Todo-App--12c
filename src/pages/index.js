f593f59be542ced67ec6a73b6809b7378665929f
wajahatabbas27 <wajahatabbas27@gmail.com>
2021-10-01 11:51:39 +0500

    added theme-ui and .toml file step 4 completed

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,22 @@
+import { Button, Container, Flex, Heading } from "@theme-ui/components";
 import React from "react";
 
 const index = () => {
-  return <div>Hello</div>;
+  return (
+    <Container>
+      <Flex sx={{ flexDirection: "column", padding: 3 }}>
+        <Heading as='h1'>JamStack Todo App</Heading>
+        <Button
+          sx={{ marginTop: 2, color: "black" }}
+          onClick={() => {
+            alert("Clicked");
+          }}
+        >
+          Login
+        </Button>
+      </Flex>
+    </Container>
+  );
 };
 
 export default index;
---
+++ wrap-root-element.js
@@ -0,0 +1,12 @@
+const React = require("react");
+const { ThemeProvider } = require("theme-ui");
+const { dark } = require("@theme-ui/presets");
+
+const myTheme = {
+  ...dark,
+  sizes: { container: 1024 },
+};
+
+export const wrapRootElement = ({ element }) => {
+  return <ThemeProvider theme={myTheme}>{element}</ThemeProvider>;
+};
